7275cb72e866b175a77bfb701b71b6b328e0845c
1CD Band PAO - Laptop 2 <paolaptop2@Laptop-2-1CD-Band-PAO.local>
2019-06-14 14:43:11 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,14 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardThree);
-console.log("user flipped " + cardTwo);
+let cards = ['queen', 'queen', 'king', 'king'];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");}
+}
